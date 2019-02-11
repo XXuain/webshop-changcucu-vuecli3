0b280e6bf7af836066a3705988b952e310ed5a46
Yellow_huang <hlady1115@gmail.com>
2019-02-11 14:11:05 +0800

    dist fix

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    baseUrl: '/webshop-changcucu-vuecli3/dist/',
-    outputDir: undefined,
-    assetsDir: undefined,
-    runtimeCompiler: undefined,
-    productionSourceMap: undefined,
-    parallel: undefined,
-    css: undefined
+    publicPath: '/webshop-changcucu-vuecli3/dist/'
 }
