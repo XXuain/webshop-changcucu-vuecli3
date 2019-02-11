4a8bb1360d82026e2c525af955aaf3e43f0648b4
Yellow_huang <hlady1115@gmail.com>
2019-02-11 13:42:18 +0800

    vue.cinfig.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    baseUrl: '/webshop-changcucu-vuecli3/dist/',
+    outputDir: undefined,
+    assetsDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined
+}
